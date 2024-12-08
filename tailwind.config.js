f6e105748026ec23386f6d7d74a5f53c2691c64b
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-12-08 13:59:21 +0700

    chore: update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        fullgray: '#999999',
+        subtlegray: '#D8D8D8',
+        lightgray: '#6D6D6D',
       }
     },
   },
